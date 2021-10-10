4f7c8f55ef53e941a480553744a73c2cb69131a2
nguyenaman23 <91823952+nguyenaman23@users.noreply.github.com>
2021-10-10 08:01:56 +0700

    Add files via upload

---
+++ gomineall/cfg.php
@@ -0,0 +1,11 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36";
+
+$ctrx = "PHPSESSID=77601fd3f122e16f59f01ea7a0e7a6c2";
+
+$cdoge = "PHPSESSID=c4fth56plgnnicjl5rkqocrqs2";
+
+$cltc = "PHPSESSID=483beb31322547eb34468afca0d59ff5";
+
+$czec = "PHPSESSID=5h1lsvfjbvhldnfmccoal6dae5";
